c61c3f74df0189bb7ff3632792e98cbdbfd7908b
Victoria Kireitseva <victoria.kireitseva@gmail.com>
2025-03-26 15:58:08 +0300

    reformat code

--- model/data/users.py
+++ model/data/users.py
@@ -21,7 +21,8 @@ class User:
 
 
 user = User(first_name="Maria", last_name="Lopez", email="MLopez@gmail.com",
-            gender="Female", mobile_number="0123456789", birth_year="1996", birth_month="9", birth_day="10", text_birth_month = 'October',
+            gender="Female", mobile_number="0123456789", birth_year="1996", birth_month="9", birth_day="10",
+            text_birth_month='October',
             subject="Biology",
             hobbies="Reading", avatar="unnamed.jpg", address="Main street, 55 bld, 10 apt.", state="Rajasthan",
             city="Jaipur")
